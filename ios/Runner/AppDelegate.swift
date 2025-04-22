fe54c1519f8d4dc89e7b1905eb00ab2e4a7b0c5f
rmarmet <r.marmet@fldai.org>
2025-04-22 09:51:28 +0500

    SCAN | the scan process has been started, we will test it and finalize it

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,25 +1,49 @@
 import UIKit
 import Flutter
+import ARKit
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
+    
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
+        let channel = FlutterMethodChannel(name: "com.demo.channel/message",
+                                           binaryMessenger: controller.binaryMessenger)
+        
+        channel.setMethodCallHandler { [weak self] (call: FlutterMethodCall, result: @escaping FlutterResult) in
+            if call.method == "startScan" {
+                self?.startLiDARScan(result: result)
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        }
 
-    let controller = window?.rootViewController as! FlutterViewController
-    let channel = FlutterMethodChannel(name: "com.demo.channel/message",
-                                       binaryMessenger: controller.binaryMessenger)
-
-    channel.setMethodCallHandler { (call, result) in
-      if call.method == "getMessage" {
-        result("Hello from Swift!")
-      } else {
-        result(FlutterMethodNotImplemented)
-      }
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    private func startLiDARScan(result: @escaping FlutterResult) {
+        if #available(iOS 13.4, *) {
+            guard ARWorldTrackingConfiguration.supportsSceneReconstruction(.meshWithClassification) else {
+                result("Scene reconstruction not supported on this device.")
+                return
+            }
+
+            let config = ARWorldTrackingConfiguration()
+            config.planeDetection = [.horizontal, .vertical]
+            config.environmentTexturing = .automatic
+            config.sceneReconstruction = .meshWithClassification
+
+            let arSession = ARSession()
+            arSession.run(config)
+
+            print("Started LiDAR scan session.")
+            result("LiDAR scan started.")
+        } else {
+            result("Scene reconstruction requires iOS 13.4 or newer.")
+        }
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   static const platform = MethodChannel('com.demo.channel/message');
 
-  Future<String> _getMessage() async {
+  Future<String> _startScan() async {
     try {
-      final String result = await platform.invokeMethod('getMessage');
+      final String result = await platform.invokeMethod('startScan');
       return result;
     } catch (e) {
       return 'Error: ${e.toString()}';
@@ -17,11 +19,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String display = "Press the button";
+    String display = "Tap to start scan";
 
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Android + Swift Setup')),
+        appBar: AppBar(title: Text('Flutter + Swift ARKit')),
         body: Center(
           child: StatefulBuilder(
             builder: (context, setState) {
@@ -29,9 +31,9 @@ class MyApp extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   ElevatedButton(
-                    child: Text("Get Native Message"),
+                    child: Text("Start 3D Scan"),
                     onPressed: () async {
-                      String msg = await _getMessage();
+                      String msg = await _startScan();
                       setState(() => display = msg);
                     },
                   ),
